4002529f6423ca622284aa2d1c2182ae596ece27
H4wking <44340939+H4wking@users.noreply.github.com>
2019-09-25 13:36:49 +0300

    a

--- shell_sort.py
+++ shell_sort.py
@@ -1,4 +1,15 @@
 def shell_sort(lst):
+    counter = 0
+    n = len(lst)
     gap = len(lst) // 2
-    for i in range(len(lst)):
-        
+    while gap > 0:
+        for i in range(gap, n):
+            key = lst[i]
+            j = i
+            while j >= gap and lst[j - gap] > key:
+                counter += 1
+                lst[j] = lst[j - gap]
+                j -= gap
+            lst[j] = key
+        gap //= 2
+    return counter
